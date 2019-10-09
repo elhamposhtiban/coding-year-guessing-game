efc8d6d957a48a9cc6afcd3b8661e606a46ef4ae
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-10-09 18:17:55 -0500

    changing js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,11 @@
-// const result_div = document.getElementById("result1");
-// const result_div = document.getElementById("result2");
-// let userNumber = 10;
-// let counter = 0;
-// const maxTries = 3;
-// let randomNumber = Math.floor(Math.random()*userNumber)+1;
-// let Attempt = 0;
-// while (Attempt!=randomNumber ) {
-//      Attempt = prompt ("please pick a number between 1 and" +userNumber);
-//      counter +=1;
-//      if(counter>maxTries){
-//      document.write("you have no more tries you lost the game ");
-//     //  document.getElementById("try").innerHTML=
-//       }  
-//      break
-//     }
-// if (Attempt===randomNumber){
-//     document.write("congrats! you have the correct number!");
-//     document.write(" the random number is"+randomNumber);
-//     document.write("it took you"+counter+"to guess the number");
-// }
-const computerValues=[
+const maxAttemps = 3;
+const currentAttempt = 0;
+// currentAttemp <= maxAttempts --> run the quiz function
+
+const computerValues = [
     "what year was Html created?",
-    "what year was Css created?",
-   "what year was SQL created?",
+    "what year was Css created?", 
+    "what year was SQL created?",
     "what year was C++ created?",
     "what year was JavaScript created?",
     "in which year the first computer was invented?",
@@ -30,10 +13,63 @@ const computerValues=[
     "what year was PHP created?",
     "what year was Matlab created?",
     "what year was ASP.net created?",
-];
-const randomHint=computerValues[Math.floor(Math.random()*9)];
+ ];
+    const years = ["1990","1994","1970","1979","1995","1936","1989","1994","1984","2002"]
+   //computer choose question random
+    let randomHint = computerValues[Math.floor(Math.random()*9)];
+    const questionText = document.getElementById("question-text");
+    questionText.textContent = randomHint;
+    let randomAnwser = years[Math.floor(Math.random()*9)];
+    console.log(randomAnwser);
+// create underscore based on year length
+const underScore=[];
+let rightAnswer = [];
+let wrongAswer = [];
+let generateUnderscore = () =>
+ {
+    for( let i=0 ;i<4 ;i++)
+    {
+    underScore.push ("_");
+  
+    }
+    return underScore;
+ }
+console.log(generateUnderscore());
+//get user guess
+document.addEventListener("keypress",(event) => 
+{
+  let keycode = event.keyCode;
+  let keypad = String.fromCharCode(keycode);  
+ //if user guess is right
+ if(randomAnwser.indexOf(keypad)>-1)
+ {
+   // console.log(true);
+   //add to right array
+   rightAnswer.push(keypad);
+   //replace underscore with right answer
+   underScore[randomAnwser.indexOf(keypad)] = keypad;
+   if(underScore.join("")=== randomAnwser)
+   {
+      alert("you win");
+      const rightguess = document.getElementById("right-guess");
+       rightguess.textContent ="you win!" ;
+   }
+  // console.log(underScore);
+   //console.log(rightAnswer);
+}
+ else 
+ {
+    wrongAswer.push(keypad);
+    if(underScore.join("")!== randomAnwser)
+    alert("game over");
+   // console.log(wrongAswer);
+ }
+  
+});
 
-const questionText = document.getElementById("question-text");
-questionText.textContent= randomHint;
+ const remainingNumbers = randomHint.length;
+
+// display questions randomly
+
 
 
