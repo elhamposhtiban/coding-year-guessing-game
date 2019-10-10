a284be457024ffaf3bd3df99b95288ea74a35076
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-10-10 02:11:26 -0500

    push changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,94 @@
 const maxAttemps = 3;
-const currentAttempt = 0;
+let currentAttempt = 0;
 // currentAttemp <= maxAttempts --> run the quiz function
 
 const computerValues = [
-    "what year was Html created?",
-    "what year was Css created?", 
-    "what year was SQL created?",
+    "what year was Apple founded?",
+    "what year was Microsoft created?", 
     "what year was C++ created?",
-    "what year was JavaScript created?",
+    "what year was Adobe founded?",
     "in which year the first computer was invented?",
     "what year was Paython created?",
-    "what year was PHP created?",
     "what year was Matlab created?",
-    "what year was ASP.net created?",
- ];
-    const years = ["1990","1994","1970","1979","1995","1936","1989","1994","1984","2002"]
-    let question =[];
-    // here we add two arrayes computer values and years
-    for( i=0 ; i<computerValues.length; i++)
-    {
-     question [i]= computerValues[i]+ years [i];
-    }
-   //computer choose question random
-    let randomHint = computerValues[Math.floor(Math.random()*9)];
-    const questionText = document.getElementById("question-text");
-    questionText.textContent = randomHint;
-    let randomAnwser =question[Math.floor(Math.random()*9)];
-    console.log(randomAnwser);
+];
+    
+const years = ["1976","1975","1970","1982","1936","1989","1984"]    
+let question =[];
+let answer = [];
+const questionText = document.getElementById("question-text");
+console.log(questionText);
 
-const underScore=[];
+// here we add two arrays computer values and years
+i = Math.floor(Math.random()*6);
+question[i] = computerValues[i];
+answer[i] = years[i];
+console.log(question[i]);
+questionText.textContent = question[i];
+console.log(answer[i]);
+randomAnwser = answer[i];
+
+const underScore = [];
 let rightAnswer = [];
 let wrongAnswer = [];
 let underscoreText = [];
-triesText = document.getElementById("tries-text");
+
+const triesText = document.getElementById("tries-text");
 underscoreText = document.getElementsByClassName("underscores");
- rightguessText =document.getElementsByClassName("rightGuess");
- wrongguessText =document.getElementsByClassName("wrongGuess");
-// create underscore based on 4 digit length
-let generateUnderscore = () =>
- {
-    for( let i=0 ;i<4 ;i++)
-    {
-    underScore.push ("_");
-    }
-    return underScore;
- }
-//console.log(generateUnderscore());
+rightguessText = document.getElementsByClassName("rightGuess");
+wrongguessText = document.getElementsByClassName("wrongGuess");
+
+function generateUnderscore() {
+   for(let i=0; i<4; i++) {
+      underScore.push("_");
+   }
+   return underScore;
+}
+
 //get user guess
-document.addEventListener("keypress",(event) => 
-{
+document.addEventListener("keypress",(event) => {
   let keycode = event.keyCode;
   //change it to digit but type string
   let keypad = String.fromCharCode(keycode);  
-  //console.log(keypad);
- //if user guess is right
- if(randomAnwser.indexOf(keypad)>-1)
- {
-   // console.log(true);
+
+  console.log(randomAnwser.indexOf(keypad));
+ // if user's guess is right
+ if(randomAnwser.indexOf(keypad) > -1) {
    //add to right array
    rightAnswer.push(keypad);
-   //replace underscore with digit of year
-   underScore[randomAnwser.indexOf(keypad)]= keypad;
-   underscoreText[0].innerHTML= underScore.join("");
+   //replace user answer with underscore
+   underScore[randomAnwser.indexOf(keypad)] = keypad;
+  // console.log("underscoreText", underscoreText[0].innerHTML);
+   underscoreText[0].innerHTML = underScore.join("");
    rightguessText[0].innerHTML = rightAnswer;
-   //console.log(rightAnswer);
-   if(underScore.join("")=== randomAnwser)
-   {
-      // alert("you win");
+   if(underScore.join("") === randomAnwser) {
+       alert("you win");
    }
- // console.log(rightAnswer);
+
 }
- else 
- {
+ else {
     wrongAnswer.push(keypad);
     wrongguessText[0].innerHTML = wrongAnswer;
-   // console.log(wrongAswer); 
-    if(underScore.join("")!== randomAnwser)
-    {
-       for(i=0;i<maxAttemps; i++)
-       {
-         maxAttemps-=
-         triesText[0].innerHTML = maxAttemps;
-       }
-    }
-   // alert("game over");
+
+   //  if(); {
     
+   // }
+   // else {
+      
+   // }
+   if (underScore.join("") !== randomAnwser && currentAttempt <= maxAttemps) {
+      currentAttempt = currentAttempt + 1;
+      // console.log('currentAttempt', currentAttempt);
+       triesText.textContent = currentAttempt;
+   }
+    else {
+       alert("Game over!");
+   }
  }
   
 });
 
 underscoreText[0].innerHTML= generateUnderscore().join("");
 
-// display questions randomly
-
-
-
+function renew() {
+ location.reload();
+ }
